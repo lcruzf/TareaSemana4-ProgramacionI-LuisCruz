875f19f382a01774391f3c22d2c6aadb2b18539a
lcruzf <lcruzf@miumg.edu.gt>
2026-03-01 23:56:16 -0600

    Tarea terminada

---
+++ tarea3.cpp
@@ -0,0 +1,38 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre;
+    string nombreAlumno;
+    int precioUnitario;
+    int cantidadProduc,subtotal;
+    double iva,total;
+    cout << "Ingresa nombre completo del cliente: ";
+    getline(cin, nombre);
+
+    cout << "Ingresa precio del producto: ";
+    cin >> precioUnitario;
+
+    cout << "Introduce la cantidad de produtos comprados: ";
+    cin >> cantidadProduc;
+
+    cout << "Ingresa nombre completo del alumno: ";
+    cin >>nombreAlumno;
+
+    subtotal = precioUnitario * cantidadProduc;
+    iva = subtotal * 0.12;
+    total = subtotal + iva;
+
+    cout << fixed << setprecision(2);
+    cout << "Nombre: " << nombre << endl;
+    cout << "Precio: " << precioUnitario << endl;
+    cout << "Cantidad: " << cantidadProduc << endl;
+    cout << "Subtotal: " << subtotal << endl;
+    cout << "Iva: " << iva << endl;
+    cout << "Total: " << total << endl;
+
+    cout <<"Desarrollado por: " <<nombreAlumno << endl;
+
+    return 0;
+}
